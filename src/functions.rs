f7baf5d7665242e4f79643315dae69d20a755ffd
Vaishnav-Sabari-Girish <forgamesonly684@gmail.com>
2025-08-28 22:37:53 +0530

    Added MPU9250

--- src/functions.rs
+++ src/functions.rs
@@ -4,3 +4,60 @@
 pub mod mpu9250 {
     pub use crate::mpu9250::*;
 }
+
+use embedded_hal::i2c::I2c;
+use crate::error::Error;
+
+// Struct that holds function pointers for the MPU9250
+#[cfg(feature = "mpu9250")]
+pub struct MPU9250Functions<I2C, E> {
+    pub verify_identity : fn(&mut mpu9250::Mpu9250<I2C>) -> Result<(), Error<E>>,
+    pub configure_power : fn(&mut mpu9250::Mpu9250<I2C>) -> Result<(), Error<E>>,
+    pub setup_accelerometer : fn(&mut mpu9250::Mpu9250<I2C>, mpu9250::AccelRange) -> Result<(), Error<E>>,
+    pub setup_gyroscope : fn(&mut mpu9250::Mpu9250<I2C>, mpu9250::GyroRange) -> Result<(), Error<E>>,
+    pub initialize_sensor : fn(&mut mpu9250::Mpu9250<I2C>, mpu9250::AccelRange, mpu9250::GyroRange) -> Result<(), Error<E>>,
+    pub read_accel_raw : fn(&mut mpu9250::Mpu9250<I2C>) -> Result<[i16; 3], Error<E>>,
+    pub read_gyro_raw : fn(&mut mpu9250::Mpu9250<I2C>) -> Result<[i16; 3], Error<E>>,
+    pub read_temp_raw : fn(&mut mpu9250::Mpu9250<I2C>) -> Result<i16, Error<E>>,
+    pub read_acceleration : fn(&mut mpu9250::Mpu9250<I2C>) -> Result<[f32; 3], Error<E>>,
+    pub read_angular_velocity : fn(&mut mpu9250::Mpu9250<I2C>) -> Result<[f32; 3], Error<E>>,
+    pub read_temperature_celsius : fn(&mut mpu9250::Mpu9250<I2C>) -> Result<f32, Error<E>>,
+    pub set_sample_rate : fn(&mut mpu9250::Mpu9250<I2C>, u8) -> Result<(), Error<E>>,
+    pub set_dlpf_config : fn(&mut mpu9250::Mpu9250<I2C>, mpu9250::DlpfConfig) -> Result<(), Error<E>>,
+    pub enter_sleep_mode : fn(&mut mpu9250::Mpu9250<I2C>)-> Result<(), Error<E>>,
+    pub wake_up : fn(&mut mpu9250::Mpu9250<I2C>) -> Result<(), Error<E>>,
+}
+
+// Main struct holding all sensor function structs
+pub struct HayasenFunctions<I2C, E> {
+    #[cfg(feature = "mpu9250")]
+    pub mpu9250: MPU9250Functions<I2C, E>,
+}
+
+impl<I2C, E> HayasenFunctions<I2C, E> 
+where 
+    I2C: I2c<Error = E>
+{
+    pub fn new() -> Self {
+        HayasenFunctions {
+            #[cfg(feature = "mpu9250")]
+            mpu9250 : MPU9250Functions {
+                        verify_identity: mpu9250::Mpu9250::verify_identity,
+                        configure_power : mpu9250::Mpu9250::configure_power,
+                        setup_accelerometer : mpu9250::Mpu9250::setup_accelerometer,
+                        setup_gyroscope : mpu9250::Mpu9250::setup_gyroscope,
+                        initialize_sensor : mpu9250::Mpu9250::initialize_sensor,
+                        read_accel_raw : mpu9250::Mpu9250::read_accel_raw,
+                        read_gyro_raw : mpu9250::Mpu9250::read_gyro_raw,
+                        read_temp_raw : mpu9250::Mpu9250::read_temp_raw,
+                        read_acceleration : mpu9250::Mpu9250::read_acceleration,
+                        read_angular_velocity : mpu9250::Mpu9250::read_angular_velocity,
+                        read_temperature_celsius : mpu9250::Mpu9250::read_temperature_celsius,
+                        set_sample_rate : mpu9250::Mpu9250::set_sample_rate,
+                        set_dlpf_config : mpu9250::Mpu9250::set_dlpf_config,
+                        enter_sleep_mode : mpu9250::Mpu9250::enter_sleep_mode,
+                        wake_up : mpu9250::Mpu9250::wake_up,
+            }
+        }
+    }    
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,83 @@
 #![no_main]
 
 
+//! Example usage documentation
+//! # Examples
+//!
+//! ```rust,ignore
+//! use hayasen::prelude::*;
+//! use embedded_hal::i2c::I2c;
+//!
+//! fn main() -> Result<(), Error<MyI2CError>> {
+//!     // Easy initialization
+//!     let mut mpu = HayasenFunctions::create_mpu9250_default(i2c, 0x68)?;
+//!
+//!     // Quick readings 
+//!     let acceleration = HayasenFunctions::read_acceleration(&mut mpu)?;
+//!     let gyroscope = HayasenFunctions::read_angular_velocity(&mut mpu)?;
+//!
+//!     // Or read everything at once
+//!     let (temp, accel, gyro) = HayasenFunctions::read_all(&mut mpu)?;
+//!
+//!     Ok(())
+//! }
+//!
+//! ```
+
+
 pub mod error;
 pub mod functions;
 pub mod mpu9250;
+
+pub use error::Error;
+pub use functions::HayasenFunctions;
+
+// Prelude module for convinient imports
+pub mod prelude {
+    pub use crate::error::Error;
+    pub use crate::functions::HayasenFunctions;
+    pub use crate::mpu9250::{Mpu9250, AccelRange, GyroRange, DlpfConfig};
+    pub use embedded_hal::i2c::I2c;
+}
+
+// Wrapper function for MPU9250 
+#[cfg(feature = "mpu9250")]
+impl<I2C, E> HayasenFunctions<I2C, E>  
+where 
+    I2C : embedded_hal::i2c::I2c<Error = E>
+{
+    pub fn create_mpu9250_default(i2c : I2C, address : u8) -> Result<mpu9250::Mpu9250<I2C>, Error<E>> {
+        let mut sensor = mpu9250::Mpu9250::new(i2c, address);
+        sensor.initialize_sensor(
+            mpu9250::AccelRange::Range2G,
+            mpu9250::GyroRange::Range250Dps,
+        )?;
+        Ok(sensor)
+    }
+
+    pub fn read_accel(sensor : &mut mpu9250::Mpu9250<I2C>) -> Result<[f32; 3], Error<E>> {
+        let accel_fun = HayasenFunctions::new();
+        (accel_fun.mpu9250.read_acceleration)(sensor)
+    }
+
+    pub fn read_gyro(sensor : &mut mpu9250::Mpu9250<I2C>) -> Result<[f32; 3], Error<E>> {
+        let gyro_fun = HayasenFunctions::new();
+        (gyro_fun.mpu9250.read_angular_velocity)(sensor)
+    }
+
+    pub fn read_temp_c(sensor : &mut mpu9250::Mpu9250<I2C>) -> Result<f32, Error<E>> {
+        let temp_fun = HayasenFunctions::new();
+        (temp_fun.mpu9250.read_temperature_celsius)(sensor)
+    }
+
+    // Read all data at once (optional)
+    pub fn read_all(sensor : &mut mpu9250::Mpu9250<I2C>) -> Result<(f32, [f32; 3], [f32; 3]), Error<E>> {
+        let all_fun = HayasenFunctions::new();
+        let temp = (all_fun.mpu9250.read_temperature_celsius)(sensor)?;
+        let accel = (all_fun.mpu9250.read_acceleration)(sensor)?;
+        let gyro = (all_fun.mpu9250.read_angular_velocity)(sensor)?;
+        Ok((temp, accel, gyro))
+    }
+}
+
 
